1c028c9172e115e19c289324ce93b8fcca7fd572
unknown <valera.sant@mail.ru>
2023-07-09 15:39:45 +0300

    задача 64

---
+++ Seminar9(Zadacha64)/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static System.Console;
+Clear();
+WriteLine("Введите M:");
+int m = int.Parse(ReadLine()!);
+
+WriteLine("Введите N:");
+int n = int.Parse(ReadLine()!);
+
+WriteLine(PrintNumbers(m, n));
+
+string PrintNumbers(int m, int n)
+{
+    if (n == m|| n == 1)
+    {
+
+        return n.ToString();
+    }
+    string s = PrintNumbers(m,n - 1)+","+ n.ToString();
+
+    return s;
+}
