4a85c6928d2766959d9960ab3ec09ad8bae5119d
unknown <valera.sant@mail.ru>
2023-07-09 15:51:02 +0300

    задача 66

---
+++ Seminar9(Zadacha66)/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static System.Console;
+Clear();
+WriteLine("Введите A:");
+int a = int.Parse(ReadLine()!);
+
+WriteLine("Введите B:");
+int b = int.Parse(ReadLine()!);
+
+
+WriteLine(PrintNumbers(a, b));
+int PrintNumbers(int a, int b)
+{
+    if (b == a|| b == 1)
+    {
+
+        return 1;
+    
+    }
+    int s = PrintNumbers(a,b - 1)+b;
+
+    return s;
+}
