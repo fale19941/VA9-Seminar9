a2299a8a1b5742ffdb7a2588815e7c1585e12913
unknown <valera.sant@mail.ru>
2023-07-09 16:00:31 +0300

    задача68

---
+++ Seminar9(Zadacha68)/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static System.Console;
+Clear();
+WriteLine("Введите A:");
+int a = int.Parse(ReadLine()!);
+
+WriteLine("Введите B:");
+int b = int.Parse(ReadLine()!);
+
+
+WriteLine(ack(a,b));
+
+int ack(int n, int m)
+{
+    while (n != 0)
+    {
+        if (m == 0) m = 1;
+        else m = ack(n,m - 1);
+        n = n - 1;
+    }
+    return m+1;
+}
+
